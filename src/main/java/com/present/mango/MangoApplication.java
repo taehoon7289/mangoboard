60a0d9490611c714b5a46f777d0dadaacf1785bc
taehoon.kim <taehoon7289@gmail.com>
2020-11-16 00:44:32 +0900

    commit

--- src/main/java/com/present/mango/MangoApplication.java
+++ src/main/java/com/present/mango/MangoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MangoApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(MangoApplication.class, args);
   }
-
 }
